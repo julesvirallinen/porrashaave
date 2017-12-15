5b6ec336f5449943d0833416715628af6980fd05
Julius Uusinarkaus <julius@uusinarkaus.com>
2017-12-15 11:10:43 +0200

    Fixes js hopefully

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require bootstrap-sprockets
 //= require_tree .
